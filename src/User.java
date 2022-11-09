faac1ccc13f16bfdea78ee62ecaff7846c6aba2f
abyblackmouth <abraham.covarrubias@live.com>
2022-11-09 02:20:53 -0800

    is created login

---
+++ src/User.java
@@ -0,0 +1,17 @@
+public class User {
+    String username;
+    String password;
+    boolean inSession;
+
+    public void User(){
+        this.username = "";
+        this.password = "";
+        this.inSession = false;
+    }
+
+    public void login(String user, String pw){
+        this.username = user;
+        this.password = pw;
+        this.inSession = true;
+    }
+}
